d94ce70b63ba2a84f9fd84dbe8fde0820c22c8fd
Legus <sjlegus@seattleschools.org>
2022-05-31 13:43:11 -0700

    Ready for kids

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,37 +5,20 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public GameObject projectilePrefab;
-    public float shotsPerSecond = 1f;
-    bool canShoot = true;
-
-    public RaycastHit2D hit;
-    public float detectionRange = 5f;
-    public LayerMask mask; //set in editor
     public Transform gun;
 
     void Update()
     {
-        hit = Physics2D.Raycast(gun.position, Vector2.left, 5f, mask);
-        if(canShoot == true && hit.transform != null)
-            StartCoroutine(Fire());
+        Fire();
     }
 
-    IEnumerator Fire()
+    void Fire()
     {
-        canShoot = false;
         GameObject bullet = Instantiate(projectilePrefab, gun.position, projectilePrefab.transform.rotation);
-        yield return new WaitForSeconds(shotsPerSecond);
-        canShoot = true;
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.blue;
-        if (hit.transform != null)
-        {
-            Gizmos.DrawWireSphere(hit.point, .25f);
-        }
-            
-        Gizmos.DrawLine(gun.position, (Vector2)gun.position + Vector2.left * detectionRange);
+ 
     }
 }
--- Assets/Scripts/RaycastEx.cs
+++ Assets/Scripts/RaycastEx.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class RaycastEx : MonoBehaviour
 {
     public float distance = 5f;
-    RaycastHit2D hit;
 
+    private void Start()
+    {
+        //Physics2D.queriesStartInColliders = false;
+    }
     void Update()
     {
-        hit = Physics2D.Raycast(transform.position, Vector2.right, distance);
+
     }
 
     private void OnDrawGizmos()
     {
-        if(hit.transform != null)
-        {
-            Gizmos.DrawWireSphere(hit.point, .25f);
-        }
+      
     }
 }
